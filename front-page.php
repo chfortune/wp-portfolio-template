3899247d91e1107840ad945fb208cad433308ef6
Charles-Henry <chfortune@users.noreply.github.com>
2026-03-20 13:50:11 -0400

    Add front-page.php for home page

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<!-- Your PHP content goes here -->
+<?php
+/* Template Name: Front Page */
+
+get_header();
+?>
+
+<div class="front-page-content">
+    <h1>Welcome to My Portfolio</h1>
+    <p>This is the home page of my portfolio.</p>
+</div>
+
+<?php
+get_footer();
+?>

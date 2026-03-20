2751605f6e654ff1174e7e2d41173d989a36e72c
Charles-Henry <chfortune@users.noreply.github.com>
2026-03-20 13:47:13 -0400

    Create index.php with default template

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Default Template
+*/
+
+get_header();
+
+?>
+
+<div class="content">
+    <h1>Hello, World!</h1>
+    <p>This is the default template for index.php.</p>
+</div>
+
+<?php
+get_footer();
+?>

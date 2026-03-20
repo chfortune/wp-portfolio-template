1ea81f22e2f2a59dc4b9f918b6e7b47a6e13a737
Charles-Henry <chfortune@users.noreply.github.com>
2026-03-20 13:51:02 -0400

    Add single.php for individual articles

---
+++ single.php
@@ -0,0 +1,12 @@
+<?php
+// single.php for individual articles
+
+get_header();
+
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <div><?php the_content(); ?></div>
+<?php endwhile; endif;
+
+get_footer();
+?>
